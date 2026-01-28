371c5bdf2e796370325b8fcdea0c754d76f8549e
Prajwal Dhungana <your-email@example.com>
2026-01-28 19:38:50 +0800

    Improved the Frontend by adding adding styling to all the html

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
-    date_created = db.Column(db.DateTime, default = datetime.timezone.utc)
+    date_created = db.Column(db.DateTime, default = datetime.now())
 
     def __repr__(self):
     # __repr__ → defines how the object is represented when printed. Example: <Task 1>.
